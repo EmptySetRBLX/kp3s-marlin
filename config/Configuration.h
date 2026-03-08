d261d2b908bacefadd057cc46c3d5efbe923d56b
EmptySet <mt@outstandingmove.com>
2026-03-07 23:36:07 -0500

    chore: try to fix the screen

--- config/Configuration.h
+++ config/Configuration.h
@@ -3107,13 +3107,13 @@
  // 320x240, 2.4", FSMC Display From MKS
  // Usually paired with MKS Robin Nano V1.2
  //
- //#define MKS_ROBIN_TFT24
+ #define MKS_ROBIN_TFT24
  
  //
  // 320x240, 2.8", FSMC Display From MKS
  // Usually paired with MKS Robin Nano V1.2
  //
- #define MKS_ROBIN_TFT28
+ //#define MKS_ROBIN_TFT28
  
  //
  // 320x240, 3.2", FSMC Display From MKS
